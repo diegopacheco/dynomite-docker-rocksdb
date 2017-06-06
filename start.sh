781dacb9c8358d23a52d77de8cd80a4412583171
Diego Pacheco <diego.pacheco.it@gmail.com>
2017-06-06 14:01:14 -0300

    FIX dyn startup issue

--- start.sh
+++ start.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 cd /ardb/src/
-./ardb-server >> ardb.log &
+/ardb/src/ardb-server >> ardb.log &
 
 mkdir /var/log/dynomite/
+cd /dynomite-$DYNOMITE_VERSION/dynomite/
 /dynomite-$DYNOMITE_VERSION/dynomite/src/dynomite --conf-file=/dynomite/conf/rocksdb_cluster_$DYNOMITE_NODE.yml -v11 -M 200000 -o /var/log/dynomite/dynomite_log.txt
